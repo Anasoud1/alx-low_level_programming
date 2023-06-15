ab45fcd7e4226bb2d8f215508b0a311b9543549a
Anasoud1 <anasoudsassi@gmail.com>
2023-06-15 19:18:32 +0100

    add file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Desciprtion : a c program to display funtion printf
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
