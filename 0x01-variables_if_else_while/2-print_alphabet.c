6d7ebe11601333d4499c983fa5b0e6cc88e8c784
Anasoud1 <anasoudsassi@gmail.com>
2023-06-16 15:58:23 +0100

    add file 2-print_alph

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program to dispaly alphabet
+ *
+ * Return: 0 (succes)
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
