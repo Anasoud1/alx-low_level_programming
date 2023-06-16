136776ee35fc98f3e4c0d0fa5b7b58611a177ecc
Anasoud1 <anasoudsassi@gmail.com>
2023-06-16 21:16:30 +0100

    add file 102-

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program that display a sequence of numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	int m = 0;
+	int p = 0;
+	int q = 0;
+
+	while (n <= 9)
+	{
+		while (m <= 9)
+		{
+			while (p <= 9)
+			{
+				while (q <= 9)
+				{
+					putchar(n + 48);
+					putchar(m + 48);
+					putchar(' ');
+					putchar(p + 48);
+					putchar(q + 48);
+					if (n == 9 && m == 8 && p == 9 && q == 9)
+						break;
+					putchar(',');
+					q++;
+				}
+				p++;
+				q = 0;
+			}
+			m++;
+			q = m + 1;
+			p = n;
+		}
+		n++;
+		m = 0;
+		p = 0;
+		q = 0;
+	}
+	putchar('\n');
+	return (0);
+}
