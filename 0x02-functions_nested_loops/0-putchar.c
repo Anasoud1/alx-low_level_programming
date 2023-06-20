ee1ca2bfedcb919c53a1807587b843a9940f125e
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 12:32:37 +0100

    add file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Description: a c program to display _putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
