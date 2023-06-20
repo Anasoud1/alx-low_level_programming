028da340166af0be2b6ff1c08b2c12d12f041442
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 19:09:58 +0100

    add file 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha - a function to cheks for alphabet charactere & number
  *@c: charatere to test
- * Return: 1 if a letter or number and 0 otherwise
+ * Return: 1 if a letter and 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	return (0);
 }
