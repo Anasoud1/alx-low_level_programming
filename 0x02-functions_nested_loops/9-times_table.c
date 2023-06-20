629a2d1f119299cba2fb4c1d76e341873d3d15b8
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 22:19:43 +0100

    add file 9-print-tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,11 @@ void times_table(void)
 {
 	int i, j, num = 0;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
+			num = i * j;
 			if ((num / 10) != 0)
 				_putchar((num / 10) + 48);
 			else
