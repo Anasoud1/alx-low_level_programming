f01328019af2ea7254b5c8c259ddecabdd110b78
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 14:37:42 +0100

    add file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - entry point
+ *
+ * _putchar - writes the charatere c to stdout
  *
  * Description: a c program to display _putchar
  *
  * Return: 0 (success)
  */
+
+#include "main.h"
+
 int main(void)
 {
-	printf("_putchar");
+	_putchar("_putchar\n");
 	return (0);
+}
+int _putchar(char c[])
+{
+	return (write(1, &c, 8));
 }
