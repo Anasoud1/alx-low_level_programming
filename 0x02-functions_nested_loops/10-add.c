01eaf29b97ca1e18b7f9382362b90be8812d0494
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 22:59:52 +0100

    add file 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+# include "mqin.h"
+/**
+ * add - a function that add two number
+ * @a: a parametre given to add
+ * @b: anoter parametre to add
+ *
+ * Return: return sum of the two numbers
+ */
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+}
+
