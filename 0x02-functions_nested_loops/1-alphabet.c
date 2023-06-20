f1935d3fa2754307f1cf415f7a277be498d7086b
Anasoud1 <anasoudsassi@gmail.com>
2023-06-20 16:22:14 +0100

    add file 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * print_alphabet - print alphabet with no argument - no return
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+}
