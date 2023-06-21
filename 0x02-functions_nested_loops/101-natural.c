cb5278d17c433cb10c69bcc16f36a1de6a2ccc83
Anasoud1 <anasoudsassi@gmail.com>
2023-06-21 16:18:01 +0100

    add file 101-

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,7 @@ int main(void)
 
 	for (i = 1; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
-			s = s + i;
-		if ((i % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			s = s + i;
 	}
 	printf("%d\n", s);
