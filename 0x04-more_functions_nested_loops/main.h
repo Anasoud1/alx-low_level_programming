078a244d58cb685ffdf03a0be44df16ed2760bfb
Anasoud1 <anasoudsassi@gmail.com>
2023-06-22 14:20:19 +0100

    add file 7-print_diagonal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,3 +10,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+
+#endif
