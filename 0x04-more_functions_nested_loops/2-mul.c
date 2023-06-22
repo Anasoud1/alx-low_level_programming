76feaf397867a9b10439c836cbe51985f16cfebb
Anasoud1 <anasoudsassi@gmail.com>
2023-06-22 12:15:53 +0100

    add file 2-mil.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a function that multuply two integer
+ * @a: firt parametre to multiply
+ * @b: second parametre to multiply
+ *
+ * Return: value of the prod
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
