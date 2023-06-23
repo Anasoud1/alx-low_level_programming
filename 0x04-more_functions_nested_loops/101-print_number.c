ebee53e12b6fc58a6352851995674bbbc9c0bf2b
Anasoud1 <anasoudsassi@gmail.com>
2023-06-23 18:35:45 +0100

    add file 101-

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,33 +8,12 @@
  */
 void print_number(int n)
 {
-	int i, number;
-	int pourcentage = 1;
-	int length = 0;
-
 	if (n < 0)
 	{
-	n = -n;
-	_putchar('-');
+		_putchar('-');
+		n = -n;
 	}
-	if (n >= 0)
-	{
-		if (n <= 9)
-			_putchar(n + 48);
-		else
-		{
-			number = n;
-			while (number > 0)
-			{
-				number = number / 10;
-				length++;
-			}
-			for (i = 1 ; i < length; i++)
-				pourcentage = pourcentage * 10;
-			_putchar(n / pourcentage + 48);
-			for (i = pourcentage / 10; i >= 10 ; i = i / 10)
-				_putchar((n / i) % 10 + 48);
-			_putchar(n % 10 + 48);
-		}
-	}
+	if ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar(n % 10 + 48)
 }
