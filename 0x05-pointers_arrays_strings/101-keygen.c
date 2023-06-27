7ea26cc20df5a44c69c4d365f1960d470270cc15
Anasoud1 <anasoudsassi@gmail.com>
2023-06-27 18:29:29 +0100

    add file 101-

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program to print a radom passwords
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
