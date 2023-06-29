63c0c28637a8150ed7eec422a1d3255d56585d37
Anasoud1 <anasoudsassi@gmail.com>
2023-06-29 21:43:04 +0100

    add file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < sizeof(l) ; j++)
+		for (j = 0; j < sizeof(l) / sizeof(char); j++)
 			if (s[i] == l[j] || s[i] == l[j] + 32)
 				s[i] = n[j] + 48;
 	}
