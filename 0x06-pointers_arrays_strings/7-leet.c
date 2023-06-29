ee2e054e74a5560165c65f73fa1297ee3d1980fb
Anasoud1 <anasoudsassi@gmail.com>
2023-06-29 21:35:19 +0100

    add file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,13 @@ char *leet(char *s)
 {
 	int i, j;
 	char l[] = {'A', 'E', 'O', 'T', 'l'};
-	char n[] = {'4', '3', '0', '7', '1'};
+	int n[] = {4, 3, 0, 7, 1};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < sizeof(l) ; j++)
 			if (s[i] == l[j] || s[i] == l[j] + 32)
-				s[i] = n[j];
+				s[i] = n[j] + 48;
 	}
 	return (s);
 }
