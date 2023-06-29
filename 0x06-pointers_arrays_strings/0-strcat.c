08c7ad7983143e7f22d55a96be506c7e4664b399
Anasoud1 <anasoudsassi@gmail.com>
2023-06-29 17:32:40 +0100

    add file 0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenate two string
+ * @dest: first string
+ *
+ * @src: second string to copy and add in dest
+ *
+ * Return: dest string after concatenation
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0' ; j++)
+		dest[i + j] = src[j];
+	return (dest);
+}
