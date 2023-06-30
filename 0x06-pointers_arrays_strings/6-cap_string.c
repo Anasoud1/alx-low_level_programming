0d4cff2e4231b5de059b8e325a9be647036dc357
Anasoud1 <anasoudsassi@gmail.com>
2023-06-30 15:24:24 +0100

    add file 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,12 +14,10 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < sizeof(del); j++)
+		for (j = 0; j < 12; j++)
 		{
 			if (s[i] == del[j])
 			{
-				if (del[j] == '\t')
-					s[i] == ' ';
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
 					s[i + 1] = s[i + 1] - 32;
 			}
