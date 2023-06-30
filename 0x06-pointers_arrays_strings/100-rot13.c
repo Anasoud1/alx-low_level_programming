dee2e5ad7bb3f3fd7e3fb35830ec2f22fc03fc8c
Anasoud1 <anasoudsassi@gmail.com>
2023-06-30 18:22:46 +0100

    add file 100-rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot 13
+ * @s: string to encode
+ *
+ * Return: p string after encoding
+*/
+char *rot13(char *s)
+{
+	int i, j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char l2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *p = s;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == l[j])
+			{
+				s[i} = l2[j];
+				break;
+			}
+		}
+	s[i] = '\0';
+	return (p);
+}
