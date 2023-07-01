f0b5fa5fde993994c331c14bc120cb4a1874ca80
Anasoud1 <anasoudsassi@gmail.com>
2023-07-01 20:00:07 +0100

    add file 103-

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,7 +21,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		l = i;
 	else
 		l = j;
-	if (l + 2 > size_r)
+	if (l + 1 > size_r)
 		return (0);
 	r[l] = '\0';
 	for (k = l - 1 ; k >= 0; k--)
