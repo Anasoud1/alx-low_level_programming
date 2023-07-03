b58b31cb9c2d341307601c0373f5860e5de3898b
Anasoud1 <anasoudsassi@gmail.com>
2023-07-03 19:53:39 +0100

    add file 8-print_diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagsums - function that print the sum of the two diagonals
+ * @a: string to sum from
+ * @size: size of matrice
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, s = 0, s1 = 0;
+	int prod = size * size;
+
+	for (i = 0; i < prod; i = i + size + 1)
+		s += a[i];
+	for (j = 0; j < prod - 1; j = j + size - 1)
+		s1 = -= a[j];
+	printf("%d, %d\n", s, s1);
+}
