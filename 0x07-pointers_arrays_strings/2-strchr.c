5272c49402f2981239d3de6f680675b0e57db265
Anasoud1 <anasoudsassi@gmail.com>
2023-07-03 15:04:39 +0100

    add file 2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 	int i;
 	char *p;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
@@ -20,5 +20,6 @@ char *_strchr(char *s, char c)
 			return (p);
 		}
 	}
-	return ('\0');
+	s[i] = '\0';
+	return (NULL);
 }
