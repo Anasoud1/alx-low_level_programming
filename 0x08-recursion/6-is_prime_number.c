956e5ec5de0bcf50f8ba079ddb806c77a2417878
Anasoud1 <anasoudsassi@gmail.com>
2023-07-05 19:41:55 +0100

    add file 6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,36 @@
 #include "main.h"
 /**
+ * square - function that returns square of a number
+ *@n: number to search for the square
+ * @s: a number
+ *
+ * Return: return the value of the square
+ */
+int square(int n, int s)
+{
+	if (s * s == n)
+		return (s);
+	else if (s * s < n)
+		return (square(n, s + 1));
+	else
+		return (-1);
+}
+/**
+ * _sqrt_recursion - fucntion that returns the natural square rootof a number
+ * @n: number to check
+ * Return: the value of square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
  * is_prime_number - function that return 1 if a prime number and 0 if not
  * @n: number to check
  *
  * Return: 1 if a prime number or 0 if not
  */
-int _sqrt_recursion(int n);
 int is_prime_number(int n)
 {
 	int l;
