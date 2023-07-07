9b398d970ec1d0a59f4f3486e5fa4e3f9a321e33
Anasoud1 <anasoudsassi@gmail.com>
2023-07-07 23:30:57 +0100

    add file 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,21 @@
 #include <stdio.h>
-#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * _isdigit - a function that cheks for a digit
- * @c: parametre to check
- * Return: 1 if a number and 0 otherwise
+ * check_number - check if the string is a digit
+ * @str: string
+ * Return: 0 (success)
  */
-int _isdigit(int c)
+int check_number(char *str)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-/**
- * _atoi - function that convert a string to an integer
- * @s: string to cheks for number
- * Return: converted integer
- */
-int _atoi(char *s)
-{
-	int i, len;
-	int neg = 0;
-	int prc = 1;
-	int number = 0;
-	unsigned int n;
-	int pos = 0;
-	char *p;
+	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '+')
-			pos++;
-		if (s[i] == '-')
-			neg++;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			p = &s[i];
-			break;
-		}
-	}
-	for (i = 0;  p[i] >= '0' && p[i] <= '9'; i++)
-		;
-	len = i;
-	for (i = 1; i < len ; i++)
-		prc = prc * 10;
-	for (i = 0; i < len ; i++)
-	{
-		number = number + ((p[i] - 48) * prc);
-		prc = prc / 10;
-	}
-	if (neg > pos)
-		number = -number;
-	n = number;
-	return (n);
+	for (i = 0; i < strlen(str); i++)
+		if (!isdigit(str[i]))
+			return (0);
+	return (1);
+
 }
 /**
  * main - entry point
@@ -70,12 +34,12 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!_isdigit(*argv[i]) && *argv[i] != '-')
+			if (!check_number(argv[i]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			s += _atoi(argv[i]);
+			s += atoi(argv[i]);
 		}
 		printf("%d\n", s);
 	}
