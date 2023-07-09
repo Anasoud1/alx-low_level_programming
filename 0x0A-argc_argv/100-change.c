6aba44a200a27e3109aed855001afa85794813c5
Anasoud1 <anasoudsassi@gmail.com>
2023-07-09 12:09:54 +0100

    add file 100-change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: program that prints the min number of coins
+ * @argc: size of array
+ * @argv: array of string
+ * Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int i, n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n = atoi(argv[1]);
+		i = 0;
+		while (n >= 25)
+		{
+			n -= 25;
+			i++;
+		}
+		while (n >= 10)
+		{
+			n -= 10;
+			i++;
+		}
+		while (n >= 5)
+		{
+			n -= 5;
+			i++;
+		}
+		while (n >= 1)
+		{
+			n -= 1;
+			i++;
+		}
+		printf("%d\n", i);
+	}
+	return (0);
+}
