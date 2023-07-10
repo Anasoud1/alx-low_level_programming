7d341edee7daf039b4f3ec65207b72754b2c3526
Anasoud1 <anasoudsassi@gmail.com>
2023-07-10 15:12:45 +0100

    add file 1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 	char *s;
 	int i, len = 0;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 	s = (char *)malloc(sizeof(char) * len);
-	if (str == NULL || s == NULL)
+	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		s[i] = str[i];
