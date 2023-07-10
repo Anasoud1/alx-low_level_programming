98762681fcf840143a9df836d7779cf73c24292b
Anasoud1 <anasoudsassi@gmail.com>
2023-07-10 13:49:51 +0100

    add file creat_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
-	
+
 	if (size == 0)
 		return (NULL);
-	s = (char*)malloc(size * sizeof(char));
+	s = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		s[i] = c;
 	return (s);
