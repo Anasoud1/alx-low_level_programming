0ba12a18a519b8101749fe1a1ee30d98ec866fd1
Anasoud1 <anasoudsassi@gmail.com>
2023-07-13 18:13:19 +0100

    add file 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,11 +19,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	if (ptr == 0)
-		return (NULL);
-	s = ptr;
+		free(ptr);
+	else
+	{
+		s = ptr;
+		free(ptr);
+	}
 	s = malloc(sizeof(char) * new_size);
 	if (s == 0)
 		return (NULL);
-	free(ptr);
 	return (s);
 }
