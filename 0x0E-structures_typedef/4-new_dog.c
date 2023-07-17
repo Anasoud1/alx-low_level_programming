159ca726e0bea4af66fc74fca48ea5c6bda9bc67
Anasoud1 <anasoudsassi@gmail.com>
2023-07-17 19:59:57 +0100

    add file 4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
-	dog_t dog;
+	dog_t *dog;
 
-	my_dog = &dog;
-	if (my_dog == 0)
+	if (!name || age < 0 || !owner)
 		return (NULL);
-	my_dog->name = strcpy(my_dog->name, name);
-	my_dog->age = age;
-	my_dog->owner = strcpy(my_dog->owner, owner);
-	return (my_dog);
+	dog = malloc(sizeof(dog_t));
+	if (dog == 0)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (!dog->name)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (!dog->owner)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = strcpy(dog->owner, owner);
+	return (dog);
 }
