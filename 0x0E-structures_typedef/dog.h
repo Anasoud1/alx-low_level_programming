898bcc1e4885a3005c88f2682682967dadaaecc0
Anasoud1 <anasoudsassi@gmail.com>
2023-07-17 16:14:26 +0100

    add file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - short description
+ * @name:first element
+ * @age: second element
+ * @owner: third element
+ * Decription: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
