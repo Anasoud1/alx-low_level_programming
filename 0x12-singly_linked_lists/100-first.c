ab0cc1006cd3b4f2f4c0e4dbd06439c56efdddc4
Anasoud1 <anasoudsassi@gmail.com>
2023-07-27 22:45:25 +0100

    add file 100-first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void _constructor(void) __attribute__((constructor));
  */
 void _constructor(void)
 {
-	printf("You're beat! and yest, you must allow,\n
-		I bore my house upon my back\n");
+	printf("You're beat! and yest, you must allow,\n"
+		"I bore my house upon my back\n");
 }
