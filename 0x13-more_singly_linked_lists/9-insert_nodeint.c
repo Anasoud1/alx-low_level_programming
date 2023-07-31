037450a0d5b714f5742f79ac85e53304e6a983c3
Anasoud1 <anasoudsassi@gmail.com>
2023-07-31 20:39:24 +0100

    add file 9-inser_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (idx == i + 1)
 		{
-			new_node->next = tmp->next;
+			if (tmp->next)
+				new_node->next = tmp->next;
 			tmp->next = new_node;
 			return (new_node);
 		}
