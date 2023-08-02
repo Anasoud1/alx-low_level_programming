d51c04133921d309b21a0e0797d19870f9b92411
Anasoud1 <anasoudsassi@gmail.com>
2023-08-02 17:14:48 +0100

    add file 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,16 +1,40 @@
 #include "lists.h"
 /**
+ * _rll - fucntion that reallocate an array of pointers
+ * @list: list to copy from
+ * @s: size of array
+ * @l: last element of array
+ * Return: pointer to new list
+ */
+const listint_t **_rll(const listint_t **list, size_t s, const listint_t *l)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(s * sizeof(listint_t));
+	if (!new_list)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < s - 1; i++)
+		new_list[i] = list[i];
+	new_list[i] = l;
+	free(list);
+	return (new_list);
+}
+
+/**
  * print_listint_safe - function that prints a list
  * @head: head pointer
  * Return: size of a list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int i = 0, j = 0, count = 0;
-	const listint_t **array;
+	size_t i, count = 0;
+	const listint_t **array = NULL;
 
-	array = malloc(50 * sizeof(listint_t *) + 1);
-	if (!array || !head)
+	if (!head)
 		exit(98);
 	while (head)
 	{
@@ -18,15 +42,15 @@ size_t print_listint_safe(const listint_t *head)
 		{
 			if (head == array[i])
 			{
-				printf("-> [%p] %d\n", head, head->n);
+				printf("-> [%p] %d\n", (void *)head, head->n);
 				free(array);
 				return (count);
 			}
 		}
-		array[j++] = head;
-		printf("[%p] %d\n", head, head->n);
-		head = head->next;
 		count++;
+		array = _rll(array, count, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
 	free(array);
 	return (count);
