f648d7dc8a107638ffb24b4d41f96291feab27d0
Anasoud1 <anasoudsassi@gmail.com>
2023-08-02 13:04:50 +0100

    add file 101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that prints a list
+ * @head: head pointer
+ * Return: size of a list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int i = 0, j = 0, count = 0;
+	const listint_t **array;
+
+	array = malloc(sizeof(listint_t *) + 1);
+	if (!array || !head)
+		exit(98);
+	while (head)
+	{
+		for (i = 0; i < count; i++)
+		{
+			array = realloc(array, count * (sizeof(listint_t *) + 1));
+			if (!array)
+				exit(98);
+			if (head == array[i])
+			{
+				printf("-> [%p] %d\n", head, head->n);
+				free(array);
+				return (count);
+			}
+		}
+		array[j++] = head;
+		printf("[%p] %d\n", head, head->n);
+		head = head->next;
+		count++;
+	}
+	free(array);
+	return (count);
+}
