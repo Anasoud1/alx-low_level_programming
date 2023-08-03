cc610631b5472f7c4fa7afc85f6fb9cd6e9f0acf
Anasoud1 <anasoudsassi@gmail.com>
2023-08-03 15:08:21 +0100

    add file 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,20 @@
 #include "main.h"
 /**
- * power - function that calculate power of a number
- * @n: number
- * @p: power
- * Return: number
- */
-unsigned int power(int n, unsigned int p)
-{
-	unsigned int i, power = 1;
-
-	for (i = 0; i < p; i++)
-		power *= n;
-	return (power);
-}
-/**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: string to convert
  * Return: converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, res = 0, len = strlen(b);
+	unsigned int i, res = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '1')
-			res += power(2, len - 1 - i);
-		else if (b[i] != '0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		res = 2 * res + (b[i] - '0');
 	}
 	return (res);
 }
