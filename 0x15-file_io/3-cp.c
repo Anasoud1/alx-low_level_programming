3a7ac15c03f872819025950e2715bcc9c1f51c37
Anasoud1 <anasoudsassi@gmail.com>
2023-08-07 20:00:09 +0100

    add fil 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
  */
 int main(int ac, char *av[])
 {
-	int fo1, fo2, fr, fw, fc1, fc2;
+	int fo1, fo2, fr, fc1, fc2;
 	char buffer[1024];
 
 	if (ac != 3)
@@ -19,27 +19,30 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fo2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	fo1 = open(av[1], O_RDONLY);
-	fr = read(fo1, buffer, 1024);
-	if (fo1 == -1 || fr == -1)
+	fo2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((fr = read(fo1, buffer, 1024)) > 0)
+			if (write(fo2, buffer, fr) != fr)
+					dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
+	if (fo1 == -1 || fr == -1) 
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fw = write(fo2, buffer, fr);
-	if (fo2 == -1 || fw == -1)
+	if (fo2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	fc1 = close(fo1);
 	fc2 = close(fo2);
-	if (fc1 == -1 || fc2 == -1)
+	if (fc1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fc1);
 		exit(100);
 	}
+	if (fc2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", fc1), exit(100); 
 	return (0);
 }
 
