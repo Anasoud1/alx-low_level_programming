f77a46841d3a420b0bc6b515b2f22b9df4cf9279
Anasoud1 <anasoudsassi@gmail.com>
2023-08-07 21:59:54 +0100

    add file 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,11 +19,15 @@ int main(int ac, char *av[])
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fo1 = open(av[1], O_RDONLY);
+	if (fo1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fo2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fo2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 	while (fr)
 	{
 		fr = read(fo1, buffer, 1024);
-		if (fo1 == -1 || fr == -1)
+		if (fr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			close(fo1);
@@ -33,7 +37,7 @@ int main(int ac, char *av[])
 		if (fr == 0)
 			break;
 		fw = write(fo2, buffer, fr);
-		if (fo2 == -1 || fw == -1)
+		if (fw == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 			close(fo1);
