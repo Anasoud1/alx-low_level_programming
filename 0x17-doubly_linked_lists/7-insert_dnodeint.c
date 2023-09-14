47bd3f330bd2434a571a8d7447078cbf1b0c83be
Anasoud1 <anasoudsassi@gmail.com>
2023-09-14 14:59:41 +0100

    adding 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,23 +14,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *curr = *h, curr2 = *h;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
+	if (*h == NULL || idx == 0)
+	{
+		new_node->next = curr;
+		curr->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 	while (curr)
 	{
-		if (idx == 0)
-		{
-			new_node->next = curr;
-			curr->prev = new_node;
-			*h = new_node;
-			return (new_node);
-		}
 		if (i == idx)
 		{
 			new_node->next = curr;
