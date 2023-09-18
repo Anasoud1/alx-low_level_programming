5e5b665335de094de3ea538c8950f23f8bbccd9f
Anasoud1 <anasoudsassi@gmail.com>
2023-09-18 15:12:48 +0100

    adding 100-operations.so file

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * add - fuction that add 2 number
+ * @a: first number
+ * @b: second number
+ * Return: value of the operation
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - fuction that sub 2 number
+ * @a: first number
+ * @b: second number
+ * Return: value of the operation
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - fuction that multiply 2 number
+ * @a: first number
+ * @b: second number
+ * Return: value of the operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - fuction that divide 2 number
+ * @a: first number
+ * @b: second number
+ * Return: value of the operation
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - fuction that give modulo of 2 number
+ * @a: first number
+ * @b: second number
+ * Return: value of the operation
+ */
+
+int mod(int a, int b)
+{
+	return (a % b); 
+}
