ce4d2d37ccbd10230699add13b40151c75acb3a3
Anasoud1 <anasoudsassi@gmail.com>
2023-10-05 17:50:16 +0100

    adding 5-hash_table_print.c file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j = 0, k = 0;
+
+	for (i = 0; i < 1024; i++)
+		if (ht->array[i])
+			j++;
+	printf("{");
+	for (i = 0; i < 1024; i++)
+	{
+		if (ht->array[i])
+		{
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			k++;
+			if (j != k)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
