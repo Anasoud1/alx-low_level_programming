293c68628a1829e3a4d4c995288eecf448be441c
Anasoud1 <anasoudsassi@gmail.com>
2023-10-06 12:59:12 +0100

    updating 3-hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i;
 	hash_node_t *tmp;
 
+	if (strcmp(key, "") == 0)
+		return (0);
 	i = key_index((unsigned char *)key, ht->size);
 	tmp = malloc(sizeof(hash_node_t));
 	if (!tmp)
