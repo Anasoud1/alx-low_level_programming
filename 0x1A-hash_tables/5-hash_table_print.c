62a970490f8c1839ed657003915f90aafad79e51
Anasoud1 <anasoudsassi@gmail.com>
2023-10-06 16:19:53 +0100

    updating 5-hash_table_print.c file

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, j = 0, k = 0;
+	hash_node_t *tmp;
 
 	if (!ht || !ht->array)
 		return;
@@ -19,12 +20,14 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i])
+		tmp = ht->array[i];
+		while (tmp)
 		{
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", tmp->key, tmp->value);
 			k++;
-			if (j != k)
+			if (j != k && tmp->next)
 				printf(", ");
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
