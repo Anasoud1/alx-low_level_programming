f56d28e8ef10f6c1cdef551a710720a0b4116740
Anasoud1 <anasoudsassi@gmail.com>
2023-10-06 15:02:29 +0100

    updating 3-hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,21 @@
 #include "hash_tables.h"
 
+int check_key(hash_node_t *current, const char *key, const char *value)
+{
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (!current->value)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+	return (2);
+}
 /**
  * hash_table_set - function that adds an element to the hash table
  * @ht: hash table
@@ -12,33 +28,35 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
-	hash_node_t *tmp, *current;
+	hash_node_t *tmp;
 
 	if (strcmp(key, "") == 0 || !ht || !key || !value)
 		return (0);
 	i = key_index((unsigned char *)key, ht->size);
+	if (check_key(ht->array[i], key, value) == 1)
+		return (1);
+	else if (!check_key(ht->array[i], key, value))
+		return (0);
 	tmp = malloc(sizeof(hash_node_t));
 	if (!tmp)
 		return (0);
 	tmp->key = strdup(key);
+	if (!tmp->key)
+	{
+		free(tmp);
+		return (0);
+	}
 	tmp->value = strdup(value);
+	{
+		free(tmp);
+		free(tmp->key);
+		return (0);
+	}
 	tmp->next = NULL;
 	if (ht->array[i] == NULL)
 		ht->array[i] = tmp;
 	else
 	{
-		current = ht->array[i];
-		while(current->next)
-		{
-			if (strcmp(current->next->key, key) == 0)
-			{
-				tmp->next = current->next->next;
-				free(current->next);
-				current->next = tmp;
-				return (1);
-			}
-			current = current->next;
-		}
 		tmp->next = ht->array[i];
 		ht->array[i] = tmp;
 	}
