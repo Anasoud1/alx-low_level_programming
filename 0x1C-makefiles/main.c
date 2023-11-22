2e682cfb5f1cd5f042dd9816f7c0b62fac1712f4
Anasoud1 <anasoudsassi@gmail.com>
2023-11-22 14:40:32 +0100

    adding 2-Makefile file

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	hello();
+	printf("hello main.c\n");
+	return 0;
+}
+/* School */
+/* School */
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+
+void hello()
+{
+	printf("hello school.c\n");
+}
