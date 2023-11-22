1b188e0b4aec0bb964a10a43a7e39ccbecd5e39d
Anasoud1 <anasoudsassi@gmail.com>
2023-11-22 19:44:57 +0100

    fixing 5-island_perimeter.py file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,12 +4,23 @@
 
 def island_perimeter(grid):
     '''returns the perimeter of the island described in grid'''
+    y = 0
+    p = 0
     s = 0
     for lis in grid:
+        x = 0
         for i in lis:
             if i == 1:
-                s += 1
-    if s == 1:
-        return 4
-    p = 3 * 2 + 2 * (s - 2)
-    return p
+                p = 4
+                if (x + 1 < len(lis) and lis[x + 1] == 1):
+                    p -= 1
+                if i == 1 and (y + 1 < len(grid)  and grid[y + 1][x] == 1):
+                    p -= 1
+                if x > 0 and lis[x - 1] == 1:
+                    p -= 1
+                if y > 0 and grid[y - 1][x] == 1:
+                    p -= 1
+                s += p
+            x += 1
+        y += 1
+    return s
